a5401dcacb817138a130db9d7438258a1c83e23f
mehdighallam72 <mehdighallam72@gmail.com>
2024-09-18 17:04:24 +0100

    Initial commit

---
+++ resources/js/Pages/Welcome.tsx
@@ -0,0 +1,47 @@
+import { Link, Head } from "@inertiajs/react";
+import { PageProps } from "@/types";
+import { Button } from "@/Components/ui/button";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+
+export default function Welcome({
+    auth,
+    laravelVersion,
+    phpVersion,
+}: PageProps<{ laravelVersion: string; phpVersion: string }>) {
+    const handleImageError = () => {
+        document
+            .getElementById("screenshot-container")
+            ?.classList.add("!hidden");
+        document.getElementById("docs-card")?.classList.add("!row-span-1");
+        document
+            .getElementById("docs-card-content")
+            ?.classList.add("!flex-row");
+        document.getElementById("background")?.classList.add("!hidden");
+    };
+
+    return (
+        <>
+            <Head title="Welcome" />
+            <div>
+                Hello
+                <Button> Click me</Button>
+                <Select>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Theme" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="light">Light</SelectItem>
+                        <SelectItem value="dark">Dark</SelectItem>
+                        <SelectItem value="system">System</SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
+        </>
+    );
+}
