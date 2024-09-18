2df89f1620d90b62007f29f906d4f0c81ec9e0bd
mehdighallam72 <mehdighallam72@gmail.com>
2024-09-18 23:17:40 +0100

    setup models

--- app/Models/Authenticator.php
+++ app/Models/Authenticator.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Authenticator extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+
+    protected $fillable = [
+        'email',
+        'secret_key',
+        'current_otp',
+        'user_id',
+    ];
 }
